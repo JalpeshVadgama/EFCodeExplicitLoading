bd0523417bcab7286db81afb4aa5e19d52e80a4a
jalpesh vadgama <Jalpesh@dotnetjalps.com>
2017-04-02 17:06:11 +0530

    Added collection method in C#

--- EFCoreExplicitLoading/EFCoreExplicitLoading/Program.cs
+++ EFCoreExplicitLoading/EFCoreExplicitLoading/Program.cs
@@ -13,14 +13,16 @@ namespace EFCoreExplicitLoading
                 var deaprtments = studentConext.Departments.ToList();
                 foreach(var department in deaprtments)
                 {
-                   
+                    Console.WriteLine("Before explicit loading");
                     Console.WriteLine(department.Students==null);
 
                     //loading student explicitly
-                    studentConext.Entry(department).Reference(s => s.Students).Load();
+                    studentConext.Entry(department).Collection(s => s.Students).Load();
 
+                    Console.WriteLine("After explicit loading");
                     Console.WriteLine(department.Students == null);
                     Console.WriteLine(department.Students.Count);
+                    Console.WriteLine("------------------------------------------------");
 
                 }
                 Console.ReadLine();
