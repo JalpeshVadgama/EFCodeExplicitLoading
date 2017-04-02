c17bbc733c497534e8f6f54ccd5174479666443b
jalpesh vadgama <Jalpesh@dotnetjalps.com>
2017-04-02 17:02:56 +0530

    Addded code for explict loading.

--- EFCoreExplicitLoading/EFCoreExplicitLoading/Program.cs
+++ EFCoreExplicitLoading/EFCoreExplicitLoading/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace EFCoreExplicitLoading
 {
@@ -7,7 +8,23 @@ namespace EFCoreExplicitLoading
 
         static void Main(string[] args)
         {
-            
+            using(StudentContext studentConext= new StudentContext())
+            {
+                var deaprtments = studentConext.Departments.ToList();
+                foreach(var department in deaprtments)
+                {
+                   
+                    Console.WriteLine(department.Students==null);
+
+                    //loading student explicitly
+                    studentConext.Entry(department).Reference(s => s.Students).Load();
+
+                    Console.WriteLine(department.Students == null);
+                    Console.WriteLine(department.Students.Count);
+
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
